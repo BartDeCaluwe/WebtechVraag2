8e6d5ce8e172c22b47a84c7059bf98e84e16c4b8
BartDeCaluwe <de.caluwe.bart@gmail.com>
2016-04-12 17:58:59 +0200

    Added json functionality
    Added the json file containing jokes

--- chuck_norris/jokes/views.py
+++ chuck_norris/jokes/views.py
@@ -6,9 +6,12 @@ from django.views import generic
 from .models import Joke
 
 import redis
+import random
+import json
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 joke_list = []
+file = "C:/Users/Bart/Desktop/Webtech/WebtechVraag2/Jokes.json";
 
 def fill_db():
 	r.set('joke:1', "Dit is een goede mop")
@@ -19,8 +22,9 @@ def index(request):
 	return render(request, 'quotes/index.html')
 
 def joke(request):
-	joke_list = r.keys('joke:*')
-	jokes_text = []
+	jokes = json.loads(file)
+	jokes_text = jokes[random.randint(1, jokes.length)]
+	r.set(jokes_text)
 	for j in joke_list:
 		jokes_text.append(r.get(j))
 	return render(request, 'jokes/joke.html', {'jokes': jokes_text})
